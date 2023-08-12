ef397a3b97ab36ceec0206533f2d992d01c8df15
Mohamed Alalli BILAL <92948337+MohamedAllali@users.noreply.github.com>
2023-08-12 15:57:33 +0000

    Add files via upload

--- js/scripte.js
+++ js/scripte.js
@@ -1,11 +1,11 @@
 // // toggle icon novbar
-// // let menuIcon = document.querySelector('#menu-icon');
+// let menuIcon = document.querySelector('#menu-icon');
 // let navbar = document.querySelector('.navbar');
 
-// // menuIcon.onclick = () => {
-// //     menuIcon.classList.toggle('bx-x');
-// //     navbar.classList.toggle('active');
-// // }
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// }
 
 // // scroll section
 // let sections = document.querySelectorAll('section');
@@ -38,7 +38,7 @@
 //     header.classList.toggle('sticky', window.scrollY > 100);
 
 //     // remove toggle icon and navbar when click navbar links (scroll)
-//     // menuIcon.classList.remove('bx-x');
+//     menuIcon.classList.remove('bx-x');
 //     navbar.classList.remove('active');
 
 //     // animation footer on scroll
